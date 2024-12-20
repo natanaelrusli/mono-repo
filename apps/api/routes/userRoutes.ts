1330062b1946b4deb7b1de86da4ee4003eaac0b4
Nata Nael <natanael280198@gmail.com>
2024-12-20 11:23:25 +0700

    feat: edit update user endpoint from post to put

--- apps/api/routes/userRoutes.ts
+++ apps/api/routes/userRoutes.ts
@@ -5,6 +5,6 @@ import { authMiddleware } from "../middleware/authMiddleware";
 export const userRoute = express.Router();
 
 userRoute.get("/fetch-user-data", authMiddleware, UserController.getUserData);
-userRoute.post("/update-user-data", authMiddleware, UserController.updateUser);
+userRoute.put("/update-user-data", authMiddleware, UserController.updateUser);
 userRoute.post("/create-user", UserController.createUser);
 userRoute.post("/login", UserController.loginUser);
